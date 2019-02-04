5cc3ceca36d10661e10d3e26c78cf02375aa16bf
DavidMendelovits <David.Mendelovits@gmail.com>
2019-02-04 12:34:06 -0800

    hm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import VideoPlayer from './vidjs';
+import videojs from 'video.js'
+
+const videoJsOptions = {
+  autoplay: true,
+  controls: true, 
+  width: "flex",
+  height: "flex",
+  sources: [{
+      src: "http://www.parkourtheory.com/api/browse/frog_gainer.mp4",  
+      type: "video/mp4"
+  }], 
+  plugins: {
+  //  offset: { start: 1, end: 1, restart_beginning: true }
+    framebyframe: {
+      fps: 30,
+      steps: [
+        { text: '-1', step: -1 },
+        { text: '1', step: 1 }
+      ]
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App"
+           id="player-container">
+           <div className="player">
+        <VideoPlayer {...videoJsOptions}/>
+            </div>
       </div>
     );
   }
